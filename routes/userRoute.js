a64a0098814eb9e8fea355208e0ee74e1b632cc9
Nimrod O. Abrigo <abrigonimrod@gmail.com>
2018-08-28 17:18:56 +0800

    userRoute revision

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,27 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const userController = require('../controller/userController');
 
-let userController = require('../controller/userController');
-
-/*router.get('/getUsers', function(req,res){
-    connection.query("SELECT * FROM users", function(error, rows, fields){
-        if(error){
-            res.send(error);
-        }else{
-            res.send(rows);
-        }
-    });
-});*/
-router.get('/getUsers',userController.getUsers);
-router.get('/getUserInfo/:id',userController.getUserInfo);
-/*router.get('/getUserInfo/:id', function(req,res){
-    connection.query("SELECT * FROM users WHERE id = ?",[req.params.id],function(error,rows,fields){
-        if(error){
-            res.send(error);
-        }else{
-            res.send(rows);
-        }
-    });
-});*/
+router.get('/getUsers', userController.getUsers);
+router.get('/getUserInfo/:id', userController.getUserInfo);
+router.post('',userController.insertUser);
+router.put('',userController.updateUser);
+router.delete('/:id',userController.deleteUser);
 
 module.exports = router;
